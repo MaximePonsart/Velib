c871f0b6e841640122da17da403edf8af93ff98b
lcamus <laurent.f.camus@gmail.com>
2017-03-07 15:05:13 +0100

    fonction renvoyant la dernière disponibilité connue (temps réel) des stations Velib
    # renvoit la dernière disponibilité connue (temps réel) des stations Velib
    # entrée : rien
    # sortie : le dataframe contenant les données

---
+++ getDispoStationTR.R
@@ -0,0 +1,18 @@
+# renvoit la dernière disponibilité connue (temps réel) des stations Velib
+# entrée : rien
+# sortie : le dataframe contenant les données
+getDispoStationTR <- function() {
+
+  DecauxKey <- "78b0313157762db775e02e176699871287378746"
+  DecauxContractName <- "Paris"
+  
+  UrlDecaux <-  paste("https://api.jcdecaux.com/vls/v1/stations?contract=",DecauxContractName,"&apiKey=",DecauxKey,sep="")
+  
+  library(jsonlite)
+  library(RCurl)
+  library(curl)
+  
+  stations_dl <- jsonlite::fromJSON(UrlDecaux)
+  return(stations_dl)
+  
+}
