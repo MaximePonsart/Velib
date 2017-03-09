6440631855b3b2ffbb8c2b6f3350919fa8ec9aa7
lcamus <laurent.f.camus@gmail.com>
2017-03-10 00:07:59 +0100

    App Shiny
    fichier de déclaration global

--- global.R
+++ global.R
@@ -8,12 +8,12 @@ ColorPal <- colorNumeric(scales::seq_gradient_pal(low = "#132B43", high = "#56B1
 
 #get stations
 stations<-read.csv("stations-velib-disponibilites-en-temps-reel.csv",
-                   row.names=1,sep=";")
+                   row.names=1, sep=";", fileEncoding = "UTF-8")
 position <- as.character(stations$position)
 v <- do.call('rbind',strsplit(position,',',fixed=TRUE))
 stations$longitude <- as.numeric(v[,2])
 stations$latitude <- as.numeric(v[,1])
-stations_nom <- levels(stations$name)
+stations_nom <- sort(trimws(sapply(strsplit(levels(stations$name),"-"),'[',2)))
 v <- NULL
 
 print("***fin")
