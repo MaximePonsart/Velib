88dfa7d1a17629eed8397c141c3d40a33f9e26cb
lcamus <laurent.f.camus@gmail.com>
2017-03-18 16:02:17 +0100

    sélection interactive station départ via carte

--- server.R
+++ server.R
@@ -5,10 +5,11 @@
 # http://shiny.rstudio.com
 #
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
   source("src/server_carte.R", local = TRUE)
   source("src/server_detail.R", local = TRUE)
   source("src/server_admin.R", local = TRUE)
+  source("src/server_observe.R", local = TRUE)
   
 })
