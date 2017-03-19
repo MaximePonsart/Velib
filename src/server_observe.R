248f3482551282f61b3a77a81779a4b7864d7dbd
lcamus <laurent.f.camus@gmail.com>
2017-03-19 09:43:21 +0100

    zonage carte station départ : gestion événéments

--- src/server_observe.R
+++ src/server_observe.R
@@ -1,38 +1,37 @@
-reactive({input$stationsProx})
+#proxy <- reactive({leafletProxy("carteGeo")})
+#sprox <- reactive({input$stationsProx})
+
+setMapCircleDeparr <- function (idStation) {
+  print("hello")
+  leafletProxy("carteGeo") %>%
+    addCircles(
+      color="blue",
+      lng=stations[idStation,]$longitude,
+      lat=stations[idStation,]$latitude,
+      layerId=as.vector("depart"),
+      radius=input$stationsProx)
+}
 
 observeEvent(input$carteGeo_shape_click,{
   
+  print("***1")
   event <- input$carteGeo_shape_click
   print(event$id)
   
+  setMapCircleDeparr(event$id)
   updateSelectInput(session, inputId = "stationDepart", selected=event$id)
-  
-  #deparr <- "depart"
-  #i <- ifelse(deparr=="depart", input$stationDepart, input$stationArrivee)
-  proxy <- leafletProxy("carteGeo")
-  proxy %>%
-  addCircles(
-    color="blue",
-    lng=stations[event$id,]$longitude,
-    lat=stations[event$id,]$latitude,
-    layerId=as.vector("depart"),
-    radius=input$stationsProx)
   
 })
 
-# observeEvent(input$stationsProx,{
-#   
-#   proxy <- leafletProxy("carteGeo")
-#   proxy %>%
-#     addCircles(
-#       color="blue",
-#       lng=stations[input$stationDepart,]$longitude,
-#       lat=stations[input$stationDepart,]$latitude,
-#       layerId=as.vector("depart"),
-#       radius=input$stationsProx)
-#   
-# })
+observeEvent(input$stationsProx,{
+  print("***2")
+  if (input$stationDepart != "0") setMapCircleDeparr(input$stationDepart)
+})
 
+observeEvent(input$stationDepart,{
+  print("***3")
+  if (input$stationDepart != "0") setMapCircleDeparr(input$stationDepart)
+})
 # session$onSessionEnded(function(){
 #   obs$suspend()
 # })
