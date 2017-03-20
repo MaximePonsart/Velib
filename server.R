22f03808d015f202f103de1c409512dde273a286
lcamus <laurent.f.camus@gmail.com>
2017-03-20 01:30:06 +0100

    amorce onglet trajet

--- server.R
+++ server.R
@@ -8,6 +8,7 @@
 shinyServer(function(input, output, session) {
   
   source("src/server_carte.R", local = TRUE)
+  source("src/server_parcours.R", local = TRUE)
   source("src/server_detail.R", local = TRUE)
   source("src/server_admin.R", local = TRUE)
   source("src/server_observe.R", local = TRUE)
