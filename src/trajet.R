b10ef62e591b097891f2441222e827126d7a5545
lcamus <laurent.f.camus@gmail.com>
2017-03-30 18:05:36 +0200

    intégration modèle Random forest

--- src/trajet.R
+++ src/trajet.R
@@ -135,46 +135,67 @@ getSommeDateDuree <- function(df, mat, dir, dim) {
 getPrevDispo <- function(sta, dateheure, mode) {
   
   getPrev <- function (s, dh) {
+    
     if (modele=="none") return(1)
     if (modele=="random") return(sample(0:s$bike_stands,1))
+    
     if (modele=="randomforest") {
-
-jour <- format(dh,"%w")
-jour <- as.factor(c("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi")[as.POSIXlt(dh)$wday + 1])
-levels(jour) <- levels(f_jour)
-
-heure <- format(dh,"%H")
-heure <- as.factor(as.character(as.numeric(format(Sys.time(),"%H"))-1))
-levels(heure) <- levels(f_heure)
-
-minute <- c(0,20,40)[which.min(abs(rep(as.numeric(format(dh,"%M")),3)-c(0,20,40)))]
-minute <- as.factor(minute)
-levels(minute) <- levels(f_minute)
-
-vacances <- vacances[vacances$date==format(dh, "%d/%m/%y"),]$vacance
-vacances <- as.factor(ifelse(vacances==1,"holidays","notholidays"))
-levels(vacances) <- levels(f_vacances)
-
-
-res<-predict(modeleRF[[as.numeric(s)]],
-             data.frame(jour, #jour (factor 1 à 7)
-                        heure, #heure (0 à 23)
-                        minute, #minute (0, 20, 40)
-                        meteoTemperature, #température
-                        meteoPrecipitations, #precipitations,
-                        conges #congés scolaires : 0 oui, 1 non
-             ),
-             type="response"
-)
       
-      message("!!!")
+      #jour <- format(dh,"%w")
+      jour <- as.factor(c("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi")[as.POSIXlt(dh)$wday + 1])
+      levels(jour) <- levels(f_jour)
+      
+      heure <- format(dh,"%H")
+      heure <- as.factor(as.character(as.numeric(format(Sys.time(),"%H"))-1))
+      levels(heure) <- levels(f_heure)
+      
+      minute <- c(0,20,40)[which.min(abs(rep(as.numeric(format(dh,"%M")),3)-c(0,20,40)))]
+      minute <- as.factor(minute)
+      levels(minute) <- levels(f_minute)
+      
+      temperature <- meteoTemperature
+      precipitation <- meteoPrecipitations
+      
+      vacance <- conges[conges$date==format(dh, "%d/%m/%y"),]$vacance
+      vacance <- as.factor(ifelse(vacances==1,"holidays","notholidays"))
+      levels(vacance) <- levels(f_vacance)
+      
+      s <- as.numeric(s)
+      
+      message("$$$")
+      message(s)
+      message(dh)
+      message(jour)
+      message(heure)
+      message(minute)
+      message(temperature)
+      message(precipitation)
+      message(vacance)
+      message("$$$")
+      res <- predict(modeleRF[[s]],
+                   data.frame(jour, #jour (factor 1 à 7)
+                              heure, #heure (0 à 23)
+                              minute, #minute (0, 20, 40)
+                              temperature, #température
+                              precipitation, #precipitations,
+                              vacance #congés scolaires : 0 oui, 1 non
+                   ),
+                   type="response"
+      )
+      res <- round(res)
+      
       message(res)
-      message("!!!")
       return(res)
     }
+    
   } # fin fonction getPrev
   
   #---
+  message("---")
+  message(str(sta))
+  message(str(dateheure))
+  message(mode)
+  message("---")
 
   if (mode=="bike")
     res <- data.frame(number=character(0), available_bikes=numeric(0))
@@ -182,8 +203,17 @@ res<-predict(modeleRF[[as.numeric(s)]],
     res <- data.frame(number=character(0), available_bike_stands=numeric(0))
   
   for (i in 1:nrow(sta)) {
-    p <- getPrev(sta[i,], dateheure[i,]$date_heure)
-    res <- rbind(res, c(sta[i,]$number, p))
+    message(paste0("appel getPrev : ",i))
+    message(sta[i,]$number)
+    #message(dateheure[i,]$date_heure)
+    p <- getPrev(sta[i,]$number, dateheure[i,]$date_heure)
+    message("retour getPrev")
+    if (mode=="bike")
+      res <- rbind(res, data.frame(number=sta[i,]$number, available_bikes=p))
+    else
+      res <- rbind(res, data.frame(number=sta[i,]$number, available_bike_stands=p))
+    print(res)
+    message("next")
   }
   
   if (mode=="bike")
